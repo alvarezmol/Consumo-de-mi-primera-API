9a7ff7834fe316fb7548eabc2bfa31bf864e7345
alvarezmol <86167035+alvarezmol@users.noreply.github.com>
2022-07-15 19:25:20 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const URL = "https://api.thecatapi.com/v1/images/search"
+const URL = "https://api.thecatapi.com/v1/images/search?limit=3"
 
 
 
@@ -6,8 +6,13 @@ function getNewCat() {
     fetch(URL)
     .then(res=>res.json())
     .then(data=> {
-        const img = document.querySelector("img");
-        img.src =  data[0].url;
+        const input1 = document.getElementById("foto_gatos1");
+        const input2 = document.getElementById("foto_gatos2");
+        const input3 = document.getElementById("foto_gatos3");
+        input1.src =  data[0].url;
+        input2.src =  data[1].url;
+        input3.src =  data[2].url;
     });
-  
 }
+
+getNewCat();
